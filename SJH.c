65fa9d062d45a5fc63c3ff11d8fd2a6687bc73b4
Juhwan SHIN <papagos@snu.ac.kr>
2020-12-08 23:22:00 +0900

    기본 구조체 거의 설정 완료

--- SJH.c
+++ SJH.c
@@ -5,22 +5,38 @@
 
 #define MAXIMUM_NUMBER 100
 
+// 이용자의 현재 상태를 나타냄.
+typedef enum _MemberStatus
+{
+    Normal,
+    Stopped,
+    Terminated
+}MemberStatus;
+
 typedef struct _Member
 {
     char* name[20];
     char* tel[20];
     int point;
-    short int flag;  // 현재 값이 초기값이면 0, 아니면 1
+    MemberStatus stat;  // 
 }Member;
+
+typedef struct _Transaction
+{
+    struct tm time;    // 시간을 나타내는 구조체
+    size_t ID;  // 이용자의 ID를 저장함.
+
+}Transaction;
 
 // Member의 메소드 구현
 int _Member_initialize(Member*);    // 구조체 초기화
 int _Member_addPoint(Member*, int); // 포인트 추가 함수
 int _Member_subPoint(Member*, int); // 포인트 감소 함수
-int _Member_setName(Member*, const char*);  // 이름 설정 함수
+int _Member_setName(Member*, const char*);  // name 설정 함수
 int _Member_setTel(Member*, const char*);   // tel 설정 함수
 
-const char* _Member_getName();  //
+const char* _Member_getName();  // name 반환 함수
+const char* _Member_getTel();   // tel 반환 함수
 
 
 int mainMenu(); // 메뉴번호를 return
